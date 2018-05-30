845147fbea2c37019bf3dfc604037189eefdebba
hzzhanghao2015 <hzzhanghao2015@corp.netease.com>
2018-05-30 17:17:47 +0800

    change: change audio demo1

--- audio/demo1.js
+++ audio/demo1.js
@@ -19,7 +19,7 @@ loadBtn.onclick = function () {
         audioCtx = new AudioContext();
         bufferSouce = audioCtx.createBufferSource();
         analyser = audioCtx.createAnalyser();
-        analyser.fftSize = 1024;
+        analyser.fftSize = 2048;
         bufferSouce.connect(analyser);
         analyser.connect(audioCtx.destination);
         audioCtx.decodeAudioData(request.response, function (buffer) {
@@ -64,8 +64,8 @@ function drawFrequencyGraph(analyser) {
     let width = canvas.width;
     let height = canvas.height;
     let bufferLengthAlt = analyser.frequencyBinCount;
-    // let dataArrayAlt = new Uint8Array(bufferLengthAlt);
-    let dataArrayAlt2 = new Float32Array(bufferLengthAlt);
+    let dataArrayAlt = new Uint8Array(bufferLengthAlt);
+    // let dataArrayAlt2 = new Float32Array(bufferLengthAlt);
     let barWidth = width / bufferLengthAlt;
     console.log(width, bufferLengthAlt, barWidth);
     canvasCtx.clearRect(0, 0, width, height);
@@ -74,15 +74,15 @@ function drawFrequencyGraph(analyser) {
         // https://stackoverflow.com/questions/24083349/understanding-getbytetimedomaindata-and-getbytefrequencydata-in-web-audio
         // https://stackoverflow.com/questions/44546024/why-are-the-values-returned-from-getfloatfrequencydata-negative
         // https://stackoverflow.com/questions/14169317/interpreting-web-audio-api-fft-results
-        // analyser.getByteFrequencyData(dataArrayAlt);
-        analyser.getFloatFrequencyData(dataArrayAlt2);//频率数据，返回的是db为单位的声音
-        // console.log(dataArrayAlt);
+        analyser.getByteFrequencyData(dataArrayAlt);
+        // analyser.getFloatFrequencyData(dataArrayAlt2);//频率数据，返回的是db为单位的声音
+        console.log(dataArrayAlt);
         // console.log(dataArrayAlt2);
         canvasCtx.fillStyle = 'rgb(0, 0, 0)';
         canvasCtx.fillRect(0, 0, width, height);
         for (let i = 0, x = 0; i < bufferLengthAlt; i++) {
-            // let barHeight = dataArrayAlt[i];
-            let barHeight = dataArrayAlt2[i] + 140;
+            let barHeight = dataArrayAlt[i];
+            // let barHeight = dataArrayAlt2[i] + 140;
             canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
             canvasCtx.fillRect(x, height - barHeight, barWidth, barHeight);
             x = x + barWidth;
@@ -95,14 +95,14 @@ function drawTimeGraph(analyser) {
     let width = canvas2.width;
     let height = canvas2.height;
     let bufferLength = analyser.fftSize;
-    // let dataArray = new Uint8Array(bufferLength);
-    let dataArray2 = new Float32Array(bufferLength);
+    let dataArray = new Uint8Array(bufferLength);
+    // let dataArray2 = new Float32Array(bufferLength);
     canvasCtx2.clearRect(0, 0, width, height);
     let draw = function () {
         window.drawTimeVisual = requestAnimationFrame(draw);
-        // analyser.getByteTimeDomainData(dataArray);
-        analyser.getFloatTimeDomainData(dataArray2);//波形数据
-        // console.log(dataArray);
+        analyser.getByteTimeDomainData(dataArray);
+        // analyser.getFloatTimeDomainData(dataArray2);//波形数据
+        console.log(dataArray);
         // console.log(dataArray2);
         canvasCtx2.fillStyle = 'rgb(200, 200, 200)';
         canvasCtx2.fillRect(0, 0, width, height);
@@ -111,8 +111,8 @@ function drawTimeGraph(analyser) {
         canvasCtx2.beginPath();
         let sliceWidth = width / bufferLength;
         for (let i = 0, x = 0; i < bufferLength; i++) {
-            // let y = (dataArray[i] / 128.0) * (height / 2);
-            let y = (dataArray2[i] * (height / 2)) + height / 2;
+            let y = (dataArray[i] / 128.0) * (height / 2);
+            // let y = (dataArray2[i] * (height / 2)) + height / 2;
             if (i === 0) {
                 canvasCtx2.moveTo(x, y);
             } else {
